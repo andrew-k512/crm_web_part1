c75006165983b101bc81de58ab9f9c46774028a4
andrew-k512 <andrew.fiveonetwo@gmail.com>
2017-09-18 23:04:03 -0400

    get update contact form working

--- crm.rb
+++ crm.rb
@@ -35,6 +35,30 @@ post '/contacts' do
   redirect to('/contacts')
 end
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+
 
 get '/about' do
   erb :about
